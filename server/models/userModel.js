304fa36ff18a391f5af0c274bf83494ca3447019
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-12-16 12:45:45 +0530

    first commit

---
+++ server/models/userModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+// user database scema 
+const userSchema = new mongoose.Schema({
+  name:{type:String, required:true},
+  email:{type:String, required:true,unique:true},
+  password:{type:String,required:true},
+  creditbBalance:{type:Number,default:5},
+})
+
+//it will search for user is availble or not if not true then go for second operation mongoose.model("user",userSchema) and create new user 
+const userModel =mongoose.models.user || mongoose.model("user",userSchema)
+
+export default userModel;
