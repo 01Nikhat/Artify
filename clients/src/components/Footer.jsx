2c159ee80445f2643a4d555c32ffe3871cc43f26
01Nikhat <160391952+01Nikhat@users.noreply.github.com>
2025-02-17 01:04:22 +0530

    Update Footer.jsx

--- clients/src/components/Footer.jsx
+++ clients/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <div className="flex items-center justify-between gap-4 py-3 mt-20 px-4 sm:px-10 lg:px-20">
       {/* Logo */}
-      <img src={assets.artify} alt="Logo" width={150} className="shrink-0" />
+      <img src={assets.artify} alt="Logo" width={70} className="shrink-0" />
 
       {/* Copyright (Hidden on Small Screens) */}
       <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 hidden sm:block">
